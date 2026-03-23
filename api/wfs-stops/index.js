86defd5b479f1abc8a82f36050f3dd9ff4693579
djk-OEX <djk@oexaa.com>
2026-03-23 20:06:50 +0100

    Update index.js

--- api/wfs-stops/index.js
+++ api/wfs-stops/index.js
@@ -1,12 +1,12 @@
 export default async function (context, req) {
   try {
     const url =
-      "http://midttrafik.admin.gc2.io/ows/midttrafik/rute_wfs/?" +
-      "service=WFS&" +
-      "version=1.0.0&" +
-      "request=GetFeature&" +
-      "typeName=midttrafik:stoppesteder&" +
-      "outputFormat=application/json";
+      "http://midttrafik.admin.gc2.io/ows/midttrafik/rute_wfs?" +
+      "SERVICE=WFS&" +
+      "VERSION=1.1.0&" +
+      "REQUEST=GetFeature&" +
+      "TYPENAME=rute_wfs.stoppesteder_midttrafik&" +
+      "OUTPUTFORMAT=application/json";
 
     const resp = await fetch(url, {
       headers: { accept: "application/json" }
@@ -20,17 +20,20 @@ export default async function (context, req) {
       return;
     }
 
-    const json = await resp.text();
+    const body = await resp.text();
 
     context.res = {
       status: 200,
       headers: {
-        "content-type": "application/json",
+        "content-type": "application/json; charset=utf-8",
         "cache-control": "public, max-age=60"
       },
-      body: json
+      body
     };
   } catch (err) {
-    context.res = { status: 500, body: "Proxy error: " + err.message };
+    context.res = {
+      status: 500,
+      body: "Proxy error: " + err.message
+    };
   }
 }
