ff38fc4b795e4e7fd3284df98e0954fa73eb23ae
djk-OEX <djk@oexaa.com>
2026-03-24 05:12:30 +0100

    Delete api directory

--- api/health/index.js
+++
@@ -1,6 +0,0 @@
-export default async function (context, req) {
-  context.res = {
-    headers: { "content-type": "application/json; charset=utf-8" },
-    body: JSON.stringify({ ok: true, ts: new Date().toISOString() })
-  };
-}
